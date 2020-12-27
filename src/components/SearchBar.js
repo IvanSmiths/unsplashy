80f0141749473321f1df3acb15f1ebbc3c7c3829
IvanSmiths <info@ivansmiths.com>
2020-12-27 22:32:39 +0100

    Test

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,7 +14,7 @@ class SearchBar extends Component {
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <label htmlFor="search">Search image</label>
+          <label htmlFor="search">Search image...</label>
           <input
             type="text"
             value={this.state.term}
