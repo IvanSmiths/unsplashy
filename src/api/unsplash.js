13a46dd8142200ddb4959b274daee6c3e79b7f4c
IvanSmiths <info@ivansmiths.com>
2020-12-28 17:59:20 +0100

    Api fix

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: `Client-ID D6X7kDKOe8SGnASkBxiAKgrs7uba_qBgMh5sSGLu-hU`,
+    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH}`,
   },
 });
